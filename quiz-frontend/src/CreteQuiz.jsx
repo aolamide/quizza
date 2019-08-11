07884fd4c3eb193f5981069c23610b3e3db41e35
Olamide <aboyejiolamide15@gmail.com>
2019-08-11 16:19:18 +0100

    added HTML form element

--- quiz-frontend/src/CreteQuiz.jsx
+++ quiz-frontend/src/CreteQuiz.jsx
@@ -16,27 +16,33 @@ class CreateQuiz extends Component {
 
     newQuestion = () => {
         const div = document.createElement('div');
-        div.innerHTML = '<textarea></textarea><input placeholder="OPTION A"><input placeholder="OPTION B"><input placeholder="OPTION C"><input placeholder="OPTION D"><div><label htmlFor="answer">Choose correct answer</label><select id="answer" class="answers"><option disabled>Choose Answer</option><option value="A">Option A</option><option value="B">Option B</option><option value="C">Option C</option><option value="D">Option D</option></select></div>';
+        div.innerHTML = '<textarea required class="question-input" placeholder="Question"></textarea><div class="option-row"><div class="option"><textarea required placeholder="OPTION A"></textarea></div><div class="option"><textarea required placeholder="OPTION B"></textarea></div></div><div class="option-row"><div class="option"><textarea required placeholder="OPTION C"></textarea></div><div class="option"><textarea required placeholder="OPTION D"></textarea></div></div><div><label for="answer">Choose correct answer</label><select id="answer" class="answers"><option disabled>Choose Answer</option><option value="A">Option A</option><option value="B">Option B</option><option value="C">Option C</option><option value="D">Option D</option></select><button title="Delete Question" class="delete-question">DELETE</button></div>';
         div.classList.add('question');
        const questionsBox = document.getElementById('questions');
        questionsBox.appendChild(div);
     }
 
-    saveQuestions = async () => {
+    saveQuestions = async (e) => {
+        e.preventDefault();
         let questions = await document.getElementsByClassName('question');
         console.log(questions)
         let answers = await document.querySelectorAll('select');
-        const creator = document.getElementById('name').value;
+        const creatorName = document.getElementById('name').value;
         const duration = document.getElementById('duration').value;
-        const expires = document.getElementById('expires').value;
+        const creatorEmail = document.getElementById('email').value;
+        const quizName = document.getElementById('qname').value;
+        const creator = {
+            name : creatorName,
+            email : creatorEmail
+        }
         let allQuestions = [];
         let allAnswers = [];
         for(let i = 0; i < questions.length; i++){
             let title = questions[i].children[0].value;
-            let optionA = questions[i].children[1].value;
-            let optionB = questions[i].children[2].value;
-            let optionC = questions[i].children[3].value;
-            let optionD = questions[i].children[4].value;
+            let optionA = questions[i].children[1].children[0].children[0].value;
+            let optionB = questions[i].children[1].children[1].children[0].value;
+            let optionC = questions[i].children[2].children[0].children[0].value;
+            let optionD = questions[i].children[2].children[1].children[0].value;
             let options =[optionA, optionB, optionC, optionD];
             let singleQuestion = {title : title, options : options};
             allQuestions.push(singleQuestion);
@@ -45,12 +51,15 @@ class CreateQuiz extends Component {
             allAnswers.push(answers[i].value);
         }
         this.setState({
+            name : quizName,
             questions : allQuestions,
             answers : allAnswers,
             creator : creator,
-            duration : duration,
-            expires : expires
+            duration : duration
         });
+        if(window.confirm(`Are you sure you want to submit ? \n Quiz contains ${allQuestions.length} questions `)) {
+            this.submitQuiz();
+        }
     }
 
     submitQuiz = async () => {
@@ -65,41 +74,67 @@ class CreateQuiz extends Component {
         const res = await response.json();
         alert(res);
     }
+    componentDidMount() {
+        window.addEventListener('click', e => {
+            if(e.target.classList.contains('delete-question')) {
+                e.target.parentNode.parentNode.remove()
+            }
+        })
+    }
 
     render() {
         return(
             <div>
-                <div>
-                    <label>Name : </label> <input id="name" type="text"/>
-                </div>
-                <div>
-                    <label>Duration (in seconds) : </label> <input id="duration" type="number" min="1"/>
-                </div>
-                <div>
-                    <label>Expiry Date : </label> <input id="expires" type="date" min="19/07/2019"/>
-                </div>
-                <div id="questions">
-                    <div className="question">
-                        <textarea></textarea>
-                        <input placeholder="OPTION A" />
-                        <input placeholder="OPTION B" />
-                        <input placeholder="OPTION C" />
-                        <input placeholder="OPTION D" />
-                        <div>
-                            <label htmlFor="answer">Choose correct answer</label>
-                            <select id="answer" className='answers'>
-                                <option disabled>Choose Answer</option>
-                                <option value="A">Option A</option>
-                                <option value="B">Option B</option>
-                                <option value="C">Option C</option>
-                                <option value="D">Option D</option>
-                            </select>
+                <form onSubmit={this.saveQuestions}>
+                    <div>
+                        <label>Name : </label> <input required id="name" type="text"/>
+                    </div>
+                    <div>
+                        <label>Email Address : </label> <input required id="email" type="email" />
+                    </div>
+                    <div>
+                        <label>Duration (in seconds) : </label> <input required id="duration" type="number" min="1"/>
+                    </div>
+                    <div>
+                        <label>QUIZ NAME :</label> <input required id="qname" type="text" />
+                    </div>
+                    <div id="questions">
+                        <div className="question">
+                            <textarea required className="question-input" placeholder="Question"></textarea>
+                            <div className="option-row">
+                                <div className="option">
+                                    <textarea required placeholder="OPTION A"></textarea>
+                                </div>
+                                <div className="option">
+                                    <textarea required placeholder="OPTION B"></textarea>
+                                </div>
+                                
+                            </div>
+                            <div className="option-row">
+                                <div className="option">
+                                    <textarea required placeholder="OPTION C"></textarea>
+                                </div>
+                                <div className="option">
+                                    <textarea required placeholder="OPTION D"></textarea>
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="answer">Choose correct answer</label>
+                                <select id="answer" className='answers'>
+                                    <option disabled>Choose Answer</option>
+                                    <option value="A">Option A</option>
+                                    <option value="B">Option B</option>
+                                    <option value="C">Option C</option>
+                                    <option value="D">Option D</option>
+                                </select>
+                                <button title="Delete Question" className="delete-question">DELETE</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <button onClick={this.newQuestion} >ADD</button>
-                <button onClick={this.saveQuestions}>SAVE</button>
-                <button onClick={this.submitQuiz}>SUBMIT QUIZ</button>
+                    <button onClick={this.newQuestion} >ADD</button>
+                    <button type="submit" >SUBMIT QUIZ</button>
+                    {/* <button onClick={this.submitQuiz}>SUBMIT QUIZ</button> */}
+                </form>
             </div>
         );
     }
