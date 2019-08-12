ca466d5f036c4e7cfa5e38a4355ed0a78deb4fd2
Olamide <aboyejiolamide15@gmail.com>
2019-08-12 01:12:22 +0100

    Updates

---
+++ precache-manifest.56c7e4f9285f0bb78382acbc74abb778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01405430863a0657029b099c5b39a735",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "91ba49bc93b01fbdab16",
+    "url": "/movie-app/static/css/main.d7113b90.chunk.css"
+  },
+  {
+    "revision": "7533478dfcf4e9fab9f9",
+    "url": "/movie-app/static/js/2.99db11fb.chunk.js"
+  },
+  {
+    "revision": "91ba49bc93b01fbdab16",
+    "url": "/movie-app/static/js/main.6c4a39fd.chunk.js"
+  },
+  {
+    "revision": "3a5315027bacbafbe345",
+    "url": "/movie-app/static/js/runtime~main.7dd63718.js"
+  }
+]);
