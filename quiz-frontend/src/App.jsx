4b27fd28ddb493c75885b5e87b8673c9afee1b82
Olamide <aboyejiolamide15@gmail.com>
2019-08-13 01:21:32 +0100

    remove basename from HashRouter

--- quiz-frontend/src/App.jsx
+++ quiz-frontend/src/App.jsx
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/quiz-app">
+    <Router>
       <Nav />
       <Switch>
         <Route exact path='/' component={Home} />
