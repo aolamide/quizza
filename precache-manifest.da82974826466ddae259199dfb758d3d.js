2905831caed556356541b2f0c66a4677a8837d5b
Olamide <aboyejiolamide15@gmail.com>
2019-09-09 18:24:27 +0100

    Updates

---
+++ precache-manifest.da82974826466ddae259199dfb758d3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f922a619ef027c9a69e36e81b7c521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87a9f75fd74416fdf1c5",
+    "url": "/static/css/main.d7113b90.chunk.css"
+  },
+  {
+    "revision": "cf85ba3722ac7f300535",
+    "url": "/static/js/2.5bb3ad18.chunk.js"
+  },
+  {
+    "revision": "87a9f75fd74416fdf1c5",
+    "url": "/static/js/main.0771061d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
