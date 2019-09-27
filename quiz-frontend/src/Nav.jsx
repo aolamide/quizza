f5a8af2a50a4dfbe063ff832e4b11163ff089c6d
Olamide <aboyejiolamide15@gmail.com>
2019-09-27 19:45:29 +0100

    remove unwanted links

--- quiz-frontend/src/Nav.jsx
+++ quiz-frontend/src/Nav.jsx
@@ -8,18 +8,14 @@ function Nav() {
            <Link to='/'>
             <li>Home</li>
            </Link>
-           <Link to='/about'>
-            <li>About</li>
-           </Link>
-           <Link to='/contact'>
-            <li>Contact</li>
-           </Link>
-           <Link to={`/quiz/5d304edf9f12db292c6c4f82`}>
-            <li>Quiz</li>
-           </Link>
-           <Link to='/createquiz'>
-            <li>Create Quiz</li>
-           </Link>
+           <span className="right">
+             <Link to='/howitworks'>
+              <li>How It Works</li>
+            </Link>
+            <Link to='/createquiz'>
+              <li>Create Quiz</li>
+            </Link>
+           </span>
        </ul>
    </nav>
   );
