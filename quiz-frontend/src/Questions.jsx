e16125ceae4bc326e23f156f1a0a87a79118276b
Olamide <aboyejiolamide15@gmail.com>
2019-09-27 19:45:54 +0100

    add margin top to questions

--- quiz-frontend/src/Questions.jsx
+++ quiz-frontend/src/Questions.jsx
@@ -3,7 +3,7 @@ import Question from './Question';
 
 const Questions = ({ questions }) => {
     return (
-        <div>
+        <div style={{marginTop : '50px'}}>
              {questions.map(question => <Question key={question._id}  question = {question} />)}
          </div> 
      )
