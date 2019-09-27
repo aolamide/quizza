76e40311c7b59a229a542dd239b84c9d5a2fcf03
Olamide <aboyejiolamide15@gmail.com>
2019-09-27 19:44:41 +0100

    add NavRoute for componets with nav

--- quiz-frontend/src/App.jsx
+++ quiz-frontend/src/App.jsx
@@ -2,27 +2,33 @@ import React from 'react';
 import  { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './Nav';
 import Home from './Home';
-import About from './About';
-import Contact from './Contact';
+import HowItWorks from './HowItWorks';
 import QuizPage from './QuizPage';
 import CreateQuiz from './CreteQuiz';
 import LeaderBoard from './LeaderBoard';
 import './App.css';
 
 
+const NavRoute = ({exact, path, component: Component}) => (
+  <Route exact={exact} path={path} render={(props) => (
+    <div>
+      <Nav />
+      <Component {...props}/>
+    </div>
+  )}/>
+)
+
 function App() {
   return (
-    <Router>
-      <Nav />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/contact' component={Contact} />
-        <Route exact path='/quiz/:quizId' component={QuizPage} />
-        <Route path='/createquiz' component={CreateQuiz} />
-        <Route path='/quiz/:quizId/leaderboard' component={LeaderBoard} />
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+            <NavRoute exact path='/' component={Home} />
+            <NavRoute path='/howitworks' component={HowItWorks} />
+            <NavRoute path='/createquiz' component={CreateQuiz} />
+            <Route exact path='/quiz/:quizId' component={QuizPage} />
+            <NavRoute path='/quiz/:quizId/leaderboard' component={LeaderBoard} />
+        </Switch>
+      </Router>
   );
 }
 
