62d83c35b4b6e67b286175a4fbf29dcc904444ec
Olamide <aboyejiolamide15@gmail.com>
2019-11-13 11:12:18 +0100

    Use BrowserRouter instead of HashRouter

--- quiz-frontend/src/App.jsx
+++ quiz-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import  { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import  { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './Nav';
 import Home from './Home';
 import HowItWorks from './HowItWorks';
