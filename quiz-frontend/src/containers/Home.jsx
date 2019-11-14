6cb6bc16c173569cb8fc6f98298be3ff3748a849
Olamide <aboyejiolamide15@gmail.com>
2019-11-14 08:13:35 +0100

    add style and content to homepage

--- quiz-frontend/src/containers/Home.jsx
+++ quiz-frontend/src/containers/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styles from  '../css/home.module.css';
 
 function Home() {
   document.title = 'Quizza';
   return (
     <div>
-      <h3> Home </h3>
+      <div className="mask">
+      </div>
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <div className="center">
+            <h1> Quizza </h1>
+            <i>Create fun quizzes and share with friends</i>
+          </div>
+        </div>
+          <p className={styles.creator}>Created by <a href="https://twitter.com/olamideaboyeji" target="_blank" rel="noopener noreferrer">Olamide Aboyeji </a> | {new Date().getFullYear()}</p>
+      </div>
     </div>
   );
 }
