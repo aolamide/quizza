0cf7fc25b556d857cafaebe793e7a8d712314846
Olamide <aboyejiolamide15@gmail.com>
2019-12-08 00:28:06 +0100

    rewrite timer

--- quiz-frontend/src/components/Timer.jsx
+++ quiz-frontend/src/components/Timer.jsx
@@ -1,47 +1,45 @@
 import React, {Component} from 'react';
 
 class Timer extends Component {
-    constructor(props) {
-      super()
-      this.props = props
-      this.state = {
-        min : null,
-        sec : null,
-        total : 0,
-        done : false
-      }
+  constructor(props) {
+    super()
+    this.props = props
+    this.state = {
+      min : null,
+      sec : null,
+      total : 0,
+      done : false
     }
+  }
 
   componentDidMount() {
     var total = (this.props.min * 60 ) + Number(this.props.sec);
-    this.setState({total : total});
+    window.timer = setInterval(()  => {
+      //calculations for minutes and seconds
+      var minutes = String(Math.floor((total % (60 * 60)) / 60)).padStart(2, 0);
+      var seconds = String(Math.floor((total % (60)))).padStart(2, 0);
+      
+      this.setState({
+        min : minutes, sec : seconds, total
+      }); 
+      total--;
+      // If the count down is over, auto submit
+      if (total < 0) {
+        clearInterval(window.timer);
+        this.props.timeOver();
+      }
+    }, 1000);
   }
 
   componentDidUpdate(){
     if(this.props.submitted){
-      clearInterval(this.timer);
+      clearInterval(window.timer);
     }
   }
 
   componentWillUnmount() {
-    clearInterval(this.timer);
-  }
-
-  timer = setInterval(()  => {
-    //calculations for minutes and seconds
-    var minutes = String(Math.floor((this.state.total % (60 * 60)) / 60)).padStart(2, 0);
-    var seconds = String(Math.floor((this.state.total % (60)))).padStart(2, 0);
-    
-    this.setState({
-      min : minutes, sec : seconds
-    }); 
-    this.setState({total : this.state.total - 1});
-    // If the count down is over, auto submit
-    if (this.state.total < 0) {
-      clearInterval(this.timer);
-      this.props.timeOver();
-    }
-  }, 1000);
+    clearInterval(window.timer);
+  }  
 
   render() {
     const {min, sec, total} = this.state;
