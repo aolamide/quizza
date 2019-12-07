7b01e3ac4ba95d5643ae1f124dfeebca1b2b6268
Olamide <aboyejiolamide15@gmail.com>
2019-12-08 00:29:07 +0100

    add create quiz button

--- quiz-frontend/src/containers/Home.jsx
+++ quiz-frontend/src/containers/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from  '../css/home.module.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
   document.title = 'Quizza';
@@ -10,8 +11,11 @@ function Home() {
       <div className={styles.container}>
         <div className={styles.main}>
           <div className="center">
-            <h1> Quizza </h1>
-            <i>Create fun quizzes and share with friends</i>
+            <h1 style={{fontSize: '5rem', color : '#c9e4b5', marginBottom : '5px'}}> Quizza </h1>
+            <em>Create fun quizzes and share with friends</em>
+            <Link to='/createquiz' >
+              <button className={styles.btnCreate}>CREATE QUIZ</button>
+            </Link>
           </div>
         </div>
           <p className={styles.creator}>Created by <a href="https://twitter.com/olamideaboyeji" target="_blank" rel="noopener noreferrer">Olamide Aboyeji </a> | {new Date().getFullYear()}</p>
