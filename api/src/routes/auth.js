089b229d2c67bbd2409874685edd674b51e88f9c
Olamide <aboyejiolamide15@gmail.com>
2020-06-26 00:43:06 +0100

    admin login

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { signUp, signIn, forgotPassword, confirmResetLink, updatePassword, getAllUsers} from "../controllers/auth";
+import { signUp, signIn, forgotPassword, confirmResetLink, updatePassword, getAllUsers, adminSignIn} from "../controllers/auth";
 import validate from '../validation';
 const router = express.Router();
 
 router.post('/register', validate, signUp );
 router.post('/login', signIn);
+router.post('/admin/login', adminSignIn);
 router.post('/forgotPassword', forgotPassword);
 router.get('/reset', confirmResetLink);
 router.put('/updatePassword', updatePassword);
