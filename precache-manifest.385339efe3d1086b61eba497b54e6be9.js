2f3e54abc756a223c5a3e48c668362fe7ced553d
Olamide <aboyejiolamide15@gmail.com>
2020-06-26 16:55:07 +0100

    Updates

---
+++ precache-manifest.385339efe3d1086b61eba497b54e6be9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef53772542e4063a89fb455e929f3b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2be4d7d96ccdc336f89",
+    "url": "/static/css/2.07cdd676.chunk.css"
+  },
+  {
+    "revision": "9d99c54c0eb3e1386d99",
+    "url": "/static/css/main.0c1988c3.chunk.css"
+  },
+  {
+    "revision": "e2be4d7d96ccdc336f89",
+    "url": "/static/js/2.91e21a88.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/static/js/2.91e21a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d99c54c0eb3e1386d99",
+    "url": "/static/js/main.850beb93.chunk.js"
+  },
+  {
+    "revision": "9034e0d72ea8c5b7b61b",
+    "url": "/static/js/runtime-main.a73f6bde.js"
+  },
+  {
+    "revision": "f402a910d6530fb8f4cc581ab8976885",
+    "url": "/static/media/score.f402a910.svg"
+  },
+  {
+    "revision": "31941af8b4474e297e93d5eff449a25d",
+    "url": "/static/media/test.31941af8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a05447547546f8b137b15fd739b3b4c5.js"
+  "/precache-manifest.385339efe3d1086b61eba497b54e6be9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
