61eeb399db620e29c279c0b5d4d2b55242e32663
Olamide <aboyejiolamide15@gmail.com>
2020-06-26 16:48:02 +0100

    admin delete user and get all users

--- api/src/routes/admin.js
+++ api/src/routes/admin.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getStats } from '../controllers/admin';
-import { requireSignIn, tokenValid, isAdmin} from '../controllers/auth';
+import { requireSignIn, tokenValid, isAdmin, deleteUser} from '../controllers/auth';
 
 const router = Router();
 
 router.get('/stats', requireSignIn, tokenValid, isAdmin, getStats);
+router.delete('/user/:user', requireSignIn, tokenValid, isAdmin, deleteUser);
 
 export default router;
