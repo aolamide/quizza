2f482a0cc347975fb441a8014909fb909c7a372a
Olamide <aboyejiolamide15@gmail.com>
2021-03-25 12:41:56 +0100

    update : change api link

--- src/apiBase.js
+++ src/apiBase.js
@@ -1,3 +1,1 @@
-// export default 'https://api.quizza.live/v1';
-export default 'https://quizza-test.herokuapp.com/v1';
-// export default 'http://localhost:3000/v1';
+export default 'https://api.quizza.live/v1';
